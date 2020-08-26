9ff0e70c084a19377204052be433fac45c21dec4
kiran74-ds <meetakoti.kiran@gmail.com>
2020-08-26 12:20:30 +0200

    updating model

--- DQN/code/agent.py
+++ DQN/code/agent.py
@@ -9,9 +9,9 @@ from model import QNetwork
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
-GAMMA = 0.90            # discount factor
+GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 1e-4              # learning rate 
+LR = 5e-4              # learning rate 
 UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
