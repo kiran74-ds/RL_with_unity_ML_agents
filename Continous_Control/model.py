8e3e1b6105285811a4067942aa1e4d839d26dee8
kiran74-ds <meetakoti.kiran@gmail.com>
2020-09-30 10:10:01 +0200

    adding project 2 continous control

---
+++ Continous_Control/model.py
@@ -0,0 +1,125 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
+
+class Actor(nn.Module):
+    """Estimates the policy deterministically using tanh activation for continuous action space"""
+    def __init__(self, state_size=33, action_size=4, seed=0, fc1=256, fc2=128):
+        """
+        @Param:
+        1. state_size: number of observations, i.e. brain.vector_action_space_size
+        2. action_size: number of actions, i.e. env_info.vector_observations.shape[1]
+        3. fc1: number of hidden units in the first fully connected layer. Default = 400.
+        4. fc2: number of hidden units in the second fully connected layer, default = 300.
+        """
+        super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1)
+        self.fc2 = nn.Linear(fc1, fc2) 
+        self.fc3 = nn.Linear(fc2, action_size) # µ(s|θ) {Deterministic policy}
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """
+        Resets the parameters by setting a noise from distribution following from its respective hidden unit size.
+        Format for (-fx,fx) followed from the original paper.
+        """
+        
+        f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])
+        
+        self.fc1.weight.data.uniform_(-f1, f1)
+        self.fc1.bias.data.uniform_(-f1, f1)
+
+        f2 = 1./np.sqrt(self.fc2.weight.data.size()[0])
+        self.fc2.weight.data.uniform_(-f2, f2)
+        self.fc2.bias.data.uniform_(-f2, f2)
+
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+#         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+#         self.fc1.bias.data.uniform_(*hidden_init(self.fc1))
+
+#         self.fc2.weight.data.uniform_(*hidden_init(self.fc1))
+#         self.fc2.bias.data.uniform_(*hidden_init(self.fc1))
+
+#         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        
+    def forward(self, state):
+        """
+        Performs a single forward pass to map (state,action) to policy, pi.
+        @Param:
+        1. state: current observations, shape: (env.observation_space.shape[0],)
+        2. action: immediate action to evaluate against, shape: (env.action_space.shape[0],)
+        @Return:
+        - µ(s|θ)
+        """
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return torch.tanh(self.fc3(x))
+    
+    
+    
+class Critic(nn.Module):
+    """Value approximator V(pi) as Q(s, a|θ)"""
+    def __init__(self, state_size=33, action_size=4, seed=0, fc1=256, fc2=128):
+        """
+        @Param:
+        1. state_size: number of observations, i.e. brain.vector_action_space_size
+        2. action_size: number of actions, i.e. env_info.vector_observations.shape[1]
+        3. fc1: number of hidden units in the first fully connected layer. Default = 400.
+        4. fc2: number of hidden units in the second fully connected layer, default = 300.
+        """
+        super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1)
+        self.fc2 = nn.Linear(fc1 + action_size, fc2) 
+        self.fc3 = nn.Linear(fc2, 1) #Q-value
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """
+        Resets the parameters by setting a noise from distribution following from its respective hidden unit size.
+        Format for (-fx,fx) followed from the original paper.
+        """
+        
+        f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])
+        self.fc1.weight.data.uniform_(-f1, f1)
+        self.fc1.bias.data.uniform_(-f1, f1)
+
+        f2 = 1./np.sqrt(self.fc2.weight.data.size()[0])
+        self.fc2.weight.data.uniform_(-f2, f2)
+        self.fc2.bias.data.uniform_(-f2, f2)
+
+        f3 = 3e-3
+        self.fc3.weight.data.uniform_(-f3, f3)
+        self.fc3.bias.data.uniform_(-f3, f3)
+
+#         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+#         self.fc1.bias.data.uniform_(*hidden_init(self.fc1))
+
+#         self.fc2.weight.data.uniform_(*hidden_init(self.fc1))
+#         self.fc2.bias.data.uniform_(*hidden_init(self.fc1))
+
+#         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        
+    def forward(self, state, action):
+        """
+        Performs a single forward pass to map (state,action) to Q-value
+        @Param:
+        1. state: current observations, shape: (env.observation_space.shape[0],)
+        2. action: immediate action to evaluate against, shape: (env.action_space.shape[0],)
+        @Return:
+        - q-value
+        """
+        x_s = F.relu(self.fc1(state))
+        x = torch.cat((x_s, action), dim=1) 
+        x = F.relu(self.fc2(x)) 
+        
+        return self.fc3(x) 
